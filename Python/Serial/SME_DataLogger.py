d84f24bdd4d9cb78fe4fae8c4688864b0f35c283
ismaelpob <ismael.pbt@gmail.com>
2021-05-16 11:36:55 -0400

    SME_DataLogger added

---
+++ Python/Serial/SME_DataLogger.py
@@ -0,0 +1,28 @@
+import os.path,csv
+
+class SME_DataLogger:
+    def __init__(self,in_folder_name,in_file_name,in_data_name,in_data):
+        self.folder_name = ""
+        self.file_name = ''
+        self.data = []
+        self.data_name = []
+  
+        self.data = in_data
+        self.data_name = in_data_name
+        self.folder_name = in_folder_name
+        self.file_name = in_file_name
+        
+        #Create folder if not exist
+        if not os.path.exists(self.folder_name): os.mkdir(self.folder_name)
+        if not (os.path.isfile(self.file_name)):
+            with open(self.file_name,'w',newline='') as csvfile:
+                #Create a file if not exist
+                fileWriter = csv.writer(csvfile,delimiter=',')
+                fileWriter.writerow(self.data_name)
+
+    def SME_DataLogger_SaveData(self,in_data):
+        #Save data in file
+        self.data = in_data
+        with open(self.file_name,'a+',newline='') as csvfile:
+            fileWriter = csv.writer(csvfile,delimiter=',')
+            fileWriter.writerow(self.data)
