153ac9dd1abde75faf04b4c605f4cbbcfe4d226b
ismaelPoblete <ismael.pbt@gmail.com>
2021-05-17 16:40:49 -0400

    signal.m update

--- Matlab/General/Signals.m
+++ Matlab/General/Signals.m
@@ -1,17 +1,34 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%Desing the input signal%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Create a 20 Hz signal
-f1=20;
-ts=1/100;
-T=2;
-t=0:ts:T;
-signal_20hz = sin(2*pi*f1*t);
-plot(signal_20hz);
 %Create a 1 Hz signal
-f2=1;
-signal_1hz = sin(2*pi*f2*t);
+freq=1;
+F=2;  %F=freq=2pi
+fs=100; %Samples
+t=0:1/fs:F;
+signal_hz = sin(2*pi*freq*t);
+plot(signal_hz);
+
+%Create a 1Hz signal sampling
+%{
+%F=1
+%Fs=100
+%n=0:(Fs)-1;
+%xs=sin(2*pi*F*n/Fs);
+%stem(n,xs);
+%}
+
+%Create a 1Hz and 20 Hz signal. Combine this two signals
+%{
+freq1=1
+freq2=20;
+F=2;    %F=freq=2pi
+fs=100; %Samples
+t=0:1/fs:F;
+
+signal_1hz = sin(2*pi*freq1*t);
+signal_20hz = sin(2*pi*freq2*t);
 plot(signal_1hz);
-%Combine this two signals
 signal_20hz_1hz = signal_20hz + signal_1hz;
 plot(signal_20hz_1hz);
+%}
